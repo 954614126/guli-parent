2d71f634c6b353658ac648be3c579f5585a29da9
xiaomihu <954614126lss>
2022-08-09 16:31:50 +0800

    git 练习

--- common/service_base/src/main/java/com/atguigu/servicebase/handler/Test.java
+++ common/service_base/src/main/java/com/atguigu/servicebase/handler/Test.java
@@ -3,5 +3,6 @@ package com.atguigu.servicebase.handler;
 public class Test {
     public static void main(String[] args) {
         System.out.println("------Show History----");
+        System.out.println("git 练习");
     }
 }
